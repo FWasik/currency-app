0def215c9c87d24768031edfa6f248b97af3fa51
FWasik <franekwasik2@gmail.com>
2023-04-27 17:22:30 +0200

    format change in dto

--- lib/features/currency/data/models/currency_rates_dto.dart
+++ lib/features/currency/data/models/currency_rates_dto.dart
@@ -1,5 +1,6 @@
 import 'package:currency_app/features/currency/domain/entities/currency_rate.dart';
 import 'package:decimal/decimal.dart';
+import 'package:intl/intl.dart';
 
 class CurrencyRateDTO extends CurrencyRate {
   const CurrencyRateDTO({required number, required date, required rate})
@@ -8,7 +9,7 @@ class CurrencyRateDTO extends CurrencyRate {
   factory CurrencyRateDTO.fromJson(Map<String, dynamic> json) {
     return CurrencyRateDTO(
         number: json["no"],
-        date: json["effectiveDate"],
+        date: DateFormat("MM-dd").format(DateTime.parse(json["effectiveDate"])),
         rate: Decimal.parse(json["mid"].toString()));
   }
 }
