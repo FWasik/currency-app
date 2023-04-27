79326774d74c206ac031678e723debb2e12d9d8d
FWasik <franekwasik2@gmail.com>
2023-04-27 17:20:55 +0200

    dropdown change

--- lib/features/currency/presentation/widgets/custom_dropdown.dart
+++ lib/features/currency/presentation/widgets/custom_dropdown.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 
 class CustomDropdown extends StatefulWidget {
   String dropdownValue;
-  final Function(String) onArgumentChanged;
+  final Function(String) onChanged;
+  final List<String> list;
 
   CustomDropdown(
-      {Key? key, required this.dropdownValue, required this.onArgumentChanged})
+      {Key? key,
+      required this.dropdownValue,
+      required this.onChanged,
+      required this.list})
       : super(key: key);
 
   @override
@@ -13,40 +17,44 @@ class CustomDropdown extends StatefulWidget {
 }
 
 class _CustomDropdownState extends State<CustomDropdown> {
-  List<String> list = ["Chart", "Table"];
-
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 120, vertical: 12),
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 8),
-        decoration: BoxDecoration(
-            border: Border.all(style: BorderStyle.solid),
-            borderRadius: const BorderRadius.all(Radius.circular(20))),
-        child: Center(
-          child: DropdownButton<String>(
-            value: widget.dropdownValue,
-            icon: const Icon(Icons.arrow_downward),
-            elevation: 16,
-            style: const TextStyle(color: Colors.white, fontSize: 18),
-            underline: Container(
-              height: 2,
-              color: Colors.white,
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: SizedBox(
+        width: 160,
+        child: DropdownButtonFormField(
+          decoration: InputDecoration(
+            focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide(
+                    color: Theme.of(context).primaryColorDark, width: 2)),
+            enabledBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.black, width: 2),
+              borderRadius: BorderRadius.circular(20),
             ),
-            onChanged: (String? value) {
-              setState(() {
-                widget.dropdownValue = value!;
-                widget.onArgumentChanged(value);
-              });
-            },
-            items: list.map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
+            border: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.black, width: 2),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            filled: true,
+            fillColor: Colors.black12,
           ),
+          dropdownColor: Theme.of(context).primaryColor,
+          value: widget.dropdownValue,
+          onChanged: (String? newValue) {
+            setState(() {
+              widget.dropdownValue = newValue!;
+              widget.onChanged(newValue);
+            });
+          },
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+          items: widget.list.map((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
         ),
       ),
     );
