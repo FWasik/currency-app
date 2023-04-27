e13c5a76d58e60ea10be7045a9422b4955f08b6c
FWasik <franekwasik2@gmail.com>
2023-04-27 17:22:18 +0200

    home_page changes after adding two types of charts and table

--- lib/features/currency/presentation/pages/home_page.dart
+++ lib/features/currency/presentation/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:currency_app/features/currency/presentation/bloc/currency_bloc.dart';
+import 'package:currency_app/features/currency/presentation/widgets/bar_chart_rates.dart';
+import 'package:currency_app/features/currency/presentation/widgets/line_chart_rates.dart';
 import 'package:currency_app/features/currency/presentation/widgets/currency_rate_info.dart';
-import 'package:currency_app/features/currency/presentation/widgets/chart_rates.dart';
 import 'package:currency_app/features/currency/presentation/widgets/custom_dropdown.dart';
 import 'package:currency_app/features/currency/presentation/widgets/table_rates.dart';
 import 'package:flutter/material.dart';
@@ -15,93 +16,93 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String dropdownValue = "Chart";
+  String dropdownPresentationValue = "Table";
+  String dropdownCurrValue = "EUR";
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            title: const Text(
-              "Currencies rates dashboard",
-              style: TextStyle(fontSize: 24),
-            ),
-            backgroundColor: Theme.of(context).primaryColorDark,
-            bottom: TabBar(
-                onTap: _handleTabBarTapped,
-                indicatorWeight: 10,
-                indicatorColor: Theme.of(context).primaryColorDark,
-                tabs: _tabs),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Currencies rates dashboard",
+          style: TextStyle(fontSize: 24),
+        ),
+        backgroundColor: Theme.of(context).primaryColorDark,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: ListView(children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              CustomDropdown(
+                dropdownValue: dropdownCurrValue,
+                onChanged: handleCurrencyChanged,
+                list: const ["EUR", "USD"],
+              ),
+              CustomDropdown(
+                dropdownValue: dropdownPresentationValue,
+                onChanged: handlePresentationChanged,
+                list: const ["Bar Chart", "Line Chart", "Table"],
+              ),
+            ],
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: BlocConsumer<CurrencyBloc, CurrencyState>(
-              listener: (context, state) {
-                if (state is LoadingState) {
-                  EasyLoading.show(status: "Loading...");
-                } else {
-                  EasyLoading.dismiss();
-                }
-              },
-              builder: (context, state) {
-                if (state is GetCurrencyRatesState) {
-                  return ListView(
-                    children: [
-                      CurrencyRateInfo(
-                        rate: state.rates[state.rates.length - 1],
-                      ),
-                      CustomDropdown(
-                          dropdownValue: dropdownValue,
-                          onArgumentChanged: handleArgumentChanged),
-                      dropdownValue == "Chart"
-                          ? ChartRates(rates: state.rates)
-                          : TableRates(rates: state.rates)
-                    ],
-                  );
-                } else if (state is ErrorState) {
-                  return Center(
-                    child: Text(state.error),
-                  );
-                } else {
-                  return Container();
-                }
-              },
-            ),
-          )),
+          BlocConsumer<CurrencyBloc, CurrencyState>(
+            listener: (context, state) {
+              if (state is LoadingState) {
+                EasyLoading.show(status: "Loading...");
+              } else {
+                EasyLoading.dismiss();
+              }
+            },
+            builder: (context, state) {
+              if (state is GetCurrencyRatesState) {
+                return Column(children: [
+                  CurrencyRateInfo(
+                    rate: state.rates.first,
+                    chosenCurr: dropdownCurrValue,
+                  ),
+                  if (dropdownPresentationValue == "Line Chart") ...[
+                    LineChartRates(rates: state.rates)
+                  ] else if (dropdownPresentationValue == "Bar Chart") ...[
+                    BarChartRates(rates: state.rates)
+                  ] else ...[
+                    TableRates(rates: state.rates)
+                  ]
+                ]);
+              } else if (state is ErrorState) {
+                return Center(
+                  child: Text(state.error),
+                );
+              } else {
+                return Container();
+              }
+            },
+          ),
+        ]),
+      ),
     );
   }
 
-  final List<Widget> _tabs = [
-    const Tab(
-      child: Text(
-        "EUR",
-        style: TextStyle(fontSize: 22),
-      ),
-    ),
-    const Tab(
-      child: Text(
-        "USD",
-        style: TextStyle(fontSize: 22),
-      ),
-    )
-  ];
-
-  void handleArgumentChanged(String newDropdownValue) {
+  void handlePresentationChanged(String newDropdownValue) {
     setState(() {
-      dropdownValue = newDropdownValue;
+      dropdownPresentationValue = newDropdownValue;
     });
   }
 
-  void _handleTabBarTapped(int value) {
-    switch (value) {
-      case 0:
+  void handleCurrencyChanged(String newDropdownValue) {
+    setState(() {
+      dropdownCurrValue = newDropdownValue;
+    });
+
+    switch (dropdownCurrValue) {
+      case "EUR":
         context
             .read<CurrencyBloc>()
             .add(const GetCurrencyRatesEvent(currency: "EUR"));
         break;
-      case 1:
+      case "USD":
         context
             .read<CurrencyBloc>()
             .add(const GetCurrencyRatesEvent(currency: "USD"));
