714996b3d3c690de6238c1e6f2c92b2106b41e6d
FWasik <franekwasik2@gmail.com>
2023-04-28 13:38:44 +0200

    dropping number field from dto and model

--- lib/features/currency/data/models/currency_rates_dto.dart
+++ lib/features/currency/data/models/currency_rates_dto.dart
@@ -3,12 +3,11 @@ import 'package:decimal/decimal.dart';
 import 'package:intl/intl.dart';
 
 class CurrencyRateDTO extends CurrencyRate {
-  const CurrencyRateDTO({required number, required date, required rate})
-      : super(number: number, date: date, rate: rate);
+  const CurrencyRateDTO({required date, required rate})
+      : super(date: date, rate: rate);
 
   factory CurrencyRateDTO.fromJson(Map<String, dynamic> json) {
     return CurrencyRateDTO(
-        number: json["no"],
         date: DateFormat("MM-dd").format(DateTime.parse(json["effectiveDate"])),
         rate: Decimal.parse(json["mid"].toString()));
   }
--- lib/features/currency/domain/entities/currency_rate.dart
+++ lib/features/currency/domain/entities/currency_rate.dart
@@ -2,13 +2,11 @@ import 'package:decimal/decimal.dart';
 import 'package:equatable/equatable.dart';
 
 class CurrencyRate extends Equatable {
-  final String number;
   final String date;
   final Decimal rate;
 
-  const CurrencyRate(
-      {required this.number, required this.date, required this.rate});
+  const CurrencyRate({required this.date, required this.rate});
 
   @override
-  List<Object?> get props => [number, date, rate];
+  List<Object?> get props => [date, rate];
 }
